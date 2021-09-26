e91bda68a4db240d301c8ee1dd15c5fe83d42662
ghost_man <kessal.abdel.madjid@outlook.fr>
2021-09-26 10:46:17 +0100

    video

--- app/src/main/java/com/example/agoraapp/AllUsersActivity.java
+++ app/src/main/java/com/example/agoraapp/AllUsersActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class AllUsersActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db= FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
